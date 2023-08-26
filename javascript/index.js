0b3848b1a1462d171860915bb892d3fdf5a31640
Javier Andrés <55107141+javierandres-dev@users.noreply.github.com>
2023-08-25 20:39:20 -0500

    javascript

--- javascript/index.js
+++ javascript/index.js
@@ -26,17 +26,44 @@ const operadorTernario = condicion
 const valorDado = 'anonimo';
 switch (valorDado) {
   case 'admin':
-    console.log('es administrador');
+    //console.log('es administrador');
     break;
   case 'secre':
-    console.log('es secretario');
+    //console.log('es secretario');
     break;
   case 'user':
-    console.log('es usuario');
+    //console.log('es usuario');
     break;
   default:
-    console.log('no está logueado');
+    //console.log('no está logueado');
     break;
+}
+
+// Truthy and Falsy
+let verdadero = true;
+verdadero = {};
+verdadero = [];
+verdadero = -1;
+verdadero = 1;
+verdadero = '0';
+verdadero = 'false';
+verdadero = ' ';
+verdadero = new Date();
+verdadero = Infinity;
+verdadero = -Infinity;
+if (verdadero) {
+  //console.log('el valor es Trusthy');
+}
+
+let falso = false;
+falso = 0;
+falso = -0;
+falso = '';
+falso = null;
+falso = undefined;
+falso = NaN;
+if (!falso) {
+  //console.log('el valor es Falsy');
 }
 
 /*
@@ -57,6 +84,30 @@ resultado = numero1 % numero2;
 const cadena1 = 'ab';
 const cadena2 = 'cd';
 //resultado = cadena1 + cadena2;
+let numero = 0;
+//console.log(numero);
+numero++;
+//console.log(numero);
+numero--;
+//console.log(numero);
+resultado = ++numero;
+//console.log(resultado);
+resultado = --numero;
+//console.log(resultado);
+resultado = 1 < 1;
+resultado = 1 <= 1;
+resultado = 1 > 1;
+resultado = 1 >= 1;
+resultado = 1 == 1;
+resultado = 1 == '1';
+resultado = 1 === '1';
+resultado = 'hola' < 'ola';
+resultado = 'hola' < 'bola';
+resultado = 10 === 10;
+//console.log(!resultado);
+resultado = 10 === 10 && 5 === 5;
+resultado = 10 === 10 || 5 === 6;
+//console.log(resultado);
 
 /*
 Interacción
@@ -130,3 +181,45 @@ const variableTipoConstante = null;
 //console.log(variableVar);
 //console.log(variableLet);
 //console.log(variableTipoConstante);
+
+/*
+Pedir nombre y edad al usuario y responder si es o no es mayor de edad.
+si se llama Pepita y es mayor de edad permitir ingreso a sala de espera 1,
+si se llama Pepita y es menor de edad permitir ingreso a sala de espera 2,
+si no se llama Pepita pero es mayor de edad permitir ingreso a sala de espera 3,
+sino dirigir a la sala de espera general.
+*/
+/*
+const nombre = prompt('Ingrese su nombre: ');
+const edad = parseInt(prompt('Ingrese su edad: '));
+let esMayorDeEdad = false;
+if (edad >= 18) {
+  alert(`${nombre} es mayor de edad`);
+  esMayorDeEdad = true;
+} else {
+  alert(`${nombre} es menor de edad`);
+}
+const esPepita = nombre === 'pepita' ? true : false;
+const caso =
+  esPepita && esMayorDeEdad
+    ? '1'
+    : esPepita && !esMayorDeEdad
+    ? '2'
+    : !esPepita && esMayorDeEdad
+    ? '3'
+    : '4';
+switch (caso) {
+  case '1':
+    alert(`${nombre} pasar a la sala de espera ${caso}`);
+    break;
+  case '2':
+    alert(`${nombre} pasar a la sala de espera ${caso}`);
+    break;
+  case '3':
+    alert(`${nombre} pasar a la sala de espera ${caso}`);
+    break;
+  default:
+    alert(`${nombre} pasar a la sala de espera general`);
+    break;
+}
+*/
