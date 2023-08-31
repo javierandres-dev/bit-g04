af6578f4d9bfc6b42c1f1478a9c5da8b94df8361
Javier Andrés <55107141+javierandres-dev@users.noreply.github.com>
2023-08-30 22:03:53 -0500

    course group

---
+++ docs/js/index.js
@@ -0,0 +1,12 @@
+//'use strict';
+//import getList from './modulo.js';
+
+const d = document,
+  $root = d.getElementById('root'),
+  devs = getList();
+//console.log(devs);
+let html = '';
+
+/* for (const dev of devs) {
+  console.log(dev);
+} */
---
+++ docs/js/modulo.js
@@ -0,0 +1,5 @@
+import list from '../json/index.json' assert { type: 'json' };
+
+const getList = () => list;
+
+export default getList;
