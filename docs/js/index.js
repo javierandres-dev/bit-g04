b9e0be6b8349f3fb375b917da6fbf5418fbe0a2e
Javier Andrés <55107141+javierandres-dev@users.noreply.github.com>
2023-09-06 20:38:47 -0500

    devs

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,9 +1,35 @@
 'use strict';
-import getList from './modulo.js';
-
 const d = document,
   $root = d.getElementById('root'),
-  devs = getList();
+  devs = [
+    {
+      name: 'Daniel Guevara',
+      photo:
+        'https://media.licdn.com/dms/image/D4E03AQFe9s-sqJFBdA/profile-displayphoto-shrink_400_400/0/1681144169400?e=1698883200&v=beta&t=ot-mAjBf-mE5RXbgtmWV1sHeUBkuicEOGFfD4CF8fJc',
+      web: 'https://danguevara07.github.io/bit-2023_semillero',
+      repo: 'https://github.com/DanGuevara07',
+    },
+    {
+      name: 'Luisa Castaño',
+      photo: 'https://avatars.githubusercontent.com/u/132719028?v=4',
+      web: 'https://luisacastano40.github.io/bit-2023_semillero',
+      repo: 'https://github.com/LuisaCastano40',
+    },
+    {
+      name: 'Nataly Reyes',
+      photo:
+        'https://raw.githubusercontent.com/NataRey/HojaDeVidaNata/main/img/CvNata.jpg',
+      web: 'https://natarey.github.io/bit-2023_semillero',
+      repo: 'https://github.com/NataRey',
+    },
+    {
+      name: 'Carlos Cruz',
+      photo:
+        'https://media.licdn.com/dms/image/D4E03AQECW83CD-5mIA/profile-displayphoto-shrink_200_200/0/1681140271552?e=1698883200&v=beta&t=kgWZ_mUq4q2OW9yWxGUXpjzIOt5X1rBs7Xt7cuW_qkk',
+      web: 'https://carloscruz1503.github.io/bit-2023_semillero',
+      repo: 'https://github.com/CarlosCruz1503',
+    },
+  ];
 
 let html = '';
 
@@ -23,4 +49,3 @@ for (const dev of devs) {
 }
 
 $root.innerHTML = `<div class="cards">${html}</div>`;
-localStorage.setItem('devs', JSON.stringify(devs));
--- docs/js/modulo.js
+++
@@ -1,5 +0,0 @@
-import list from '../json/index.json' assert { type: 'json' };
-
-const getList = () => list;
-
-export default getList;
